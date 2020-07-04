ff269c8dc35ce5757207c55edb882872b8535381
Niculici Cristian <niculicicris@gmail.com>
2020-07-04 20:22:58 +0300

    Second runnable

--- src/me/cristike/holoitem/events/JoinEvent.java
+++ src/me/cristike/holoitem/events/JoinEvent.java
@@ -2,6 +2,7 @@ package me.cristike.holoitem.events;
 
 import me.cristike.holoitem.HoloManager;
 import me.cristike.holoitem.Main;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class JoinEvent implements Listener {
     private Plugin plugin = Main.getPlugin(Main.class);
@@ -28,5 +30,33 @@ public class JoinEvent implements Listener {
         hm.create(p, p.getInventory().getItemInMainHand());
         hm.Move(p, Main.players.get(p.getUniqueId()));
         p.getInventory().getItemInMainHand().setAmount(amount);
+        final boolean[] initial = {true};
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Player player = p;
+                Location loc = null;
+                if (!p.isOnline()) {
+                    cancel();
+                    return;
+                }
+                if (initial[0]) {
+                    initial[0] = false;
+                    loc = player.getLocation();
+                }
+                if (player.getLocation() == loc) {
+                    if (Main.players.containsKey(player.getUniqueId())) return;
+                    if (p.getInventory().getItemInMainHand() == null || p.getInventory().getItemInMainHand().getType() == Material.AIR) return;
+                    if (plugin.getConfig().getStringList("Blacklist").contains(p.getInventory().getItemInMainHand().getType().toString().toUpperCase())) return;
+                    ItemStack nitem = player.getInventory().getItemInMainHand();
+                    int amount = nitem.getAmount();
+                    nitem.setAmount(1);
+                    hm.create(player, player.getInventory().getItemInMainHand());
+                    hm.Move(player, Main.players.get(player.getUniqueId()));
+                    player.getInventory().getItemInMainHand().setAmount(amount);
+                    Main.notMoving.add(player.getUniqueId());
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
     }
 }
